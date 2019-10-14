979458d583b0db697a5c953a8f041642cb7764b0
vermucht <vermucht@gmail.com>
2019-10-14 15:13:34 +0300

    step 2: arguments, options

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,4 +5,9 @@ import program from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig, secondConfig);
+  })
+  .option('-f, --format [type]', 'Output format')
   .parse(process.argv);

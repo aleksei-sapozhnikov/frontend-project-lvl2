c906a7070198bdbe74d0b5378d995f4ea1fadba4
vermucht <vermucht@gmail.com>
2019-10-15 09:30:15 +0300

    step 3: bin + readme

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/c1d02d4a148a3774406f/maintainability)](https://codeclimate.com/github/vermucht/frontend-project-lvl2/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/c1d02d4a148a3774406f/test_coverage)](https://codeclimate.com/github/vermucht/frontend-project-lvl2/test_coverage)
 [![Build Status](https://travis-ci.org/vermucht/frontend-project-lvl2.svg?branch=master)](https://travis-ci.org/vermucht/frontend-project-lvl2)
+
+## Install
+
+[![asciicast](https://asciinema.org/a/C5dNLIydUXMTvoNB66VqJaoQy.svg)](https://asciinema.org/a/C5dNLIydUXMTvoNB66VqJaoQy)
+
+## JSON diff
+
+[![asciicast](https://asciinema.org/a/GtBNL9FUWGo7A9QC4NVo1ybtf.svg)](https://asciinema.org/a/GtBNL9FUWGo7A9QC4NVo1ybtf)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-#!/usr/bin/env node
-
 import program from 'commander';
 import jsonDiff from './diff/jsonDiff';
 
 export default jsonDiff;
 
-program
-  .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    console.log(jsonDiff(firstConfig, secondConfig));
-  })
-  .option('-f, --format [type]', 'Output format')
-  .parse(process.argv);
+export const run = () => {
+  program
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(jsonDiff(firstConfig, secondConfig));
+    })
+    .option('-f, --format [type]', 'Output format')
+    .parse(process.argv);
+};
